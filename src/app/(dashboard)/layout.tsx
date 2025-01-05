a508a60883b483ee02e6930c2c84647669e6e0be
foungniguejp <jeanpaulsoro3@gmail.com>
2025-01-05 00:20:37 +0000

    Menu creation

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "../../components/Menu";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,9 +10,20 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-1/6 bg-red-50">L</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchooLama</span>
+        </Link>
+        <Menu />
+      </div>
       {/* RIGHT */}
-      <div className="w-5/6 bg-blue-50">R</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+        R
+      </div>
     </div>
   );
 }
